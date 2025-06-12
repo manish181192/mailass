f6f021e345bcb6fe43c148afffda0aa59c9257c8
manish181192 <manishvidyasagar@gmail.com>
2025-06-11 21:36:04 -0700

    initial commit

---
+++ src/mail_clients/gmail.py
@@ -0,0 +1,1 @@
+gmail_client_id = 1234
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from mail_clients.gmail import gmail_client_id
+
+
+def main():
+    print(f"Hello from mailass-backend! {gmail_client_id}")
+
+
+if __name__ == "__main__":
+    main()
